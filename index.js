411767ae96eb5e4e73e91ac6f7b2a0078bd62a5f
jmeno1011 <jwhltn8282@gmail.com>
2022-04-01 22:02:14 +0900

    cors추가

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = 8888;
 
-app.get('/',(req, res)=>{
+// middle ware
+app.use(cors());
+
+app.get("/", (req, res) => {
   res.status(200).send("Get data has successfully hello World!");
-})
+});
 
 app.get("/api", (req, res) => {
   try {
-    res.status(200).send("here is api")
+    res.status(200).send("here is api");
   } catch (error) {
     console.log("error :: ", error);
     return res.status(500).send("Server error");
   }
 });
 
-app.get("/test-api",(req, res)=>{
+app.get("/test-api", (req, res) => {
   res.json({
-    status:200,
-    message:"test-api-message"
-  })
-})
+    status: 200,
+    message: "test-api-message",
+  });
+});
 
 app.post("/", function (req, res) {
   res.send("Got a POST request");
